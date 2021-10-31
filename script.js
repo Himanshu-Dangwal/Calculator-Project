9114e28bc4d5b9d4dcacdbcb8442e31e7725ab7e
Himanshu Dangwal <68777129+Himanshu-Dangwal@users.noreply.github.com>
2021-10-31 23:29:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,41 +10,86 @@ function isOperator(value) {
     return value == "+" || value == "-" || value == "*" || value == "/";
 }
 
+function changes(value){
+    var text = display.textContent.trim();
+
+    if (isOperator(value)) {
+        operator = value;
+        operand1 = parseFloat(text);
+        display.textContent = "";
+    } else if (value == "ac") {
+        display.textContent = "";
+    } else if (value == "sign") {
+        operand1 = parseFloat(text);
+        operand1 = -1 * operand1;
+        display.textContent = operand1;
+    } else if (value == ".") {
+        if (text.length && !text.includes('.')) {
+            display.textContent = text + '.';
+        }
+    } else if (value == "%") {
+        operand1 = parseFloat(text);
+        operand1 = operand1 / 100;
+        display.textContent = operand1
+    } else if (value == "=") {
+        operand2 = parseFloat(text);
+        var result = eval(operand1 + ' ' + operator + ' ' + operand2);
+        if (result) {
+            display.textContent = result;
+            operand1 = result;
+            operand2 = null;
+            operator = null;
+        }
+    } else {
+        display.textContent += value;
+    }
+}
+
+document.addEventListener("keypress", function(event) {
+
+    var val = event.key;
+
+    console.log(val);
+    changes(val);
+
+  });
+
 for (var i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', function () {
 
         var value = this.getAttribute('data-value');
-        var text = display.textContent.trim();
+        // var text = display.textContent.trim();
 
-        if (isOperator(value)) {
-            operator = value;
-            operand1 = parseFloat(text);
-            display.textContent = "";
-        } else if (value == "ac") {
-            display.textContent = "";
-        } else if (value == "sign") {
-            operand1 = parseFloat(text);
-            operand1 = -1 * operand1;
-            display.textContent = operand1;
-        } else if (value == ".") {
-            if (text.length && !text.includes('.')) {
-                display.textContent = text + '.';
-            }
-        } else if (value == "%") {
-            operand1 = parseFloat(text);
-            operand1 = operand1 / 100;
-            display.textContent = operand1
-        } else if (value == "=") {
-            operand2 = parseFloat(text);
-            var result = eval(operand1 + ' ' + operator + ' ' + operand2);
-            if (result) {
-                display.textContent = result;
-                operand1 = result;
-                operand2 = null;
-                operator = null;
-            }
-        } else {
-            display.textContent += value;
-        }
+        // if (isOperator(value)) {
+        //     operator = value;
+        //     operand1 = parseFloat(text);
+        //     display.textContent = "";
+        // } else if (value == "ac") {
+        //     display.textContent = "";
+        // } else if (value == "sign") {
+        //     operand1 = parseFloat(text);
+        //     operand1 = -1 * operand1;
+        //     display.textContent = operand1;
+        // } else if (value == ".") {
+        //     if (text.length && !text.includes('.')) {
+        //         display.textContent = text + '.';
+        //     }
+        // } else if (value == "%") {
+        //     operand1 = parseFloat(text);
+        //     operand1 = operand1 / 100;
+        //     display.textContent = operand1
+        // } else if (value == "=") {
+        //     operand2 = parseFloat(text);
+        //     var result = eval(operand1 + ' ' + operator + ' ' + operand2);
+        //     if (result) {
+        //         display.textContent = result;
+        //         operand1 = result;
+        //         operand2 = null;
+        //         operator = null;
+        //     }
+        // } else {
+        //     display.textContent += value;
+        // }
+        changes(value);
     });
 }
